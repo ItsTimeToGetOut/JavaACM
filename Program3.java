d81bf876bd9db61c34e50bb03b6b6a0d87ab2afc
Atharva1012 <56938282+Atharva1012@users.noreply.github.com>
2019-10-24 02:15:19 +0530

    AtharvaProgram3.java

--- Program3.java
+++ Program3.java
@@ -1,12 +1,12 @@
-import java.io*;
+import java.io.*;
 import java.util.*;
 class Program3
 {
 public static void main(String args[])throws IOException
 {
 Scanner sc=new Scanner(System.in);
-int n=0;
-float n1=0.0f;
+int n=2;
+float n1=2.2f;
 if(n==n1)System.out.println("equal");
 else System.out.println("not equal");
 }
