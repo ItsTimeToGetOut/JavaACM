ac99fb2893326bea6ad3e78abb197269572b822d
aadityagupta27 <55345219+aadityagupta27@users.noreply.github.com>
2019-10-23 23:51:43 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -7,7 +7,7 @@ public static void main(String args[])throws IOException
 Scanner sc=new Scanner(System.in);
 int n=0;
 float n1=0.0f;
-if(n==n1)System.out.println("equal");
-else System.out.println("not equal");
+if(n==n1)System.out.println("have equal value");
+else System.out.println("do not have equal value");
 }
 }
