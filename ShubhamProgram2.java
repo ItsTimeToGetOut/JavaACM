036fe96e3206351236768b23f36245ad3416725e
Shubham0043 <55591929+Shubham0043@users.noreply.github.com>
2019-10-24 22:53:56 +0530

    Update and rename Program2.java to ShubhamProgram2.java

---
+++ ShubhamProgram2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class ShubhamProgram2
+{
+	public static void main(String args[]) throws IOException
+	{
+		int a=3;
+		double b=3;
+		System.out.println("CODE TO DIFFERENTIATE BETWEN STORAGE OF INTEGER AND DECIMAL VALUES");
+		System.out.println(a+5);
+    	System.out.println(b+5);
+	}
+}
