6fa9d1085150c9b68dff4e1fc7cc5b2f1bdaa40f
udit050500 <57041421+udit050500@users.noreply.github.com>
2019-10-26 21:24:29 +0530

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -4,9 +4,9 @@ class Program4
 {
 	public static void main(String args[]) throws IOException
 	{
-		boolean flag=false;
-    if(flag)flag=false;
-    else flag=true;
+		boolean flag=true;
+    if(flag)flag=true;
+    else flag=false;
 		System.out.println(flag);
 	}
 }
